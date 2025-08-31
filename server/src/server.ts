ea9bc1f0253b8f846426c866ea33f6774a98a969
Sol Oliveira <sole-oliveira@hotmail.com>
2025-08-31 00:29:26 -0300

    feat: configurar prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,17 @@
 import fastify from 'fastify' 
+import { PrismaClient } from '@prisma/client'
  
 const app = fastify() 
+const prisma = new PrismaClient()
  
 app.get('/ping', () => { 
     return 'Fastify is running!'; 
 }) 
+
+app.get('/products', () => {
+  const products = prisma.product.findMany()
+  return products
+})
  
 app.listen({ 
     port: 3333, 
