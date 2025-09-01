d9f9cddd6a403950e9ad9537719907c802b47f27
Sol Oliveira <sole-oliveira@hotmail.com>
2025-09-01 09:53:30 -0300

    feat: adicionar usecase sku

---
+++ server/src/application/use-case/sku/find-all-skus-use-case.ts
@@ -0,0 +1,16 @@
+import { SKUEntity } from '../../../domain/entities/sku.entity.js';
+import { ISKURepository } from '../../../domain/repositories/sku.repository.interface.js';
+
+export class FindAllSKUsUseCase {
+  private readonly skuRepository: ISKURepository;
+
+  constructor(skuRepository: ISKURepository) {    
+    this.skuRepository = skuRepository;
+  }
+
+  async execute(): Promise<SKUEntity[]> {    
+    console.log('Executando o caso de uso para buscar todos os skus...');
+    const skus = await this.skuRepository.findAll();
+    return skus;
+  }
+}
