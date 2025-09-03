08db39309e3bf697707bf1082a8bd57b66fcd69d
Sole Oliveira <sole-oliveira@hotmail.com>
2025-09-02 21:25:18 -0300

    feat: adicionar suporte para Cors e declaração de tipos para TS

--- server/src/presentation/server.ts
+++ server/src/presentation/server.ts
@@ -1,20 +1,24 @@
-import 'dotenv/config';
-import Fastify, { FastifyInstance } from 'fastify';
-import { PrismaClient } from '@prisma/client';
-
-import { skuRoutes } from './routes/sku.routes';
-import { SKURepository } from '../infrastructure/repository/sku/sku.repository';
-
+import "dotenv/config";
+import Fastify, { FastifyInstance } from "fastify";
+import { PrismaClient } from "@prisma/client";
+import { skuRoutes } from "./routes/sku.routes";
+import { SKURepository } from "../infrastructure/repository/sku/sku.repository";
+import cors from "@fastify/cors";
 
 const app: FastifyInstance = Fastify({ logger: true });
+
+app.register(cors, {
+  origin: true,
+});
+
 const prisma = new PrismaClient();
 
-app.get('/health', async () => {
+app.get("/health", async () => {
   try {
     await prisma.$queryRaw`SELECT 1`;
-    return { status: 'Fastify is running!', db: 'ok' };
-  } catch (err: any ) {
-    return { status: 'Fastify is running!', db: 'error', error: err.message };
+    return { status: "Fastify is running!", db: "ok" };
+  } catch (err: any) {
+    return { status: "Fastify is running!", db: "error", error: err.message };
   }
 });
 
@@ -22,8 +26,8 @@ app.register(skuRoutes, { skuRepository: new SKURepository(prisma) });
 
 const start = async () => {
   try {
-    await app.listen({ port: 3333, host: '0.0.0.0' });
-    app.log.info('HTTP server running on http://localhost:3333');
+    await app.listen({ port: 3333, host: "0.0.0.0" });
+    app.log.info("HTTP server running on http://localhost:3333");
   } catch (err) {
     app.log.error(err);
     process.exit(1);
@@ -31,12 +35,12 @@ const start = async () => {
 };
 
 const shutdown = async () => {
-  app.log.info('Shutting down server...');
+  app.log.info("Shutting down server...");
   await prisma.$disconnect();
   process.exit();
 };
 
-process.on('SIGINT', shutdown);
-process.on('SIGTERM', shutdown);
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
 
 start();
