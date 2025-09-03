8c9589551f21beaf2b3391ad1f7fa53c7e59e6dd
Sole Oliveira <sole-oliveira@hotmail.com>
2025-09-03 19:04:25 -0300

    style: corrigir espaçamentos

--- web/src/components/skus/SKUTableComponent.tsx
+++ web/src/components/skus/SKUTableComponent.tsx
@@ -1,5 +1,6 @@
-import { SKUEntity } from "@/types/sku";
+import { SKUEntity, SKUStatusEnum } from "@/types/sku";
 import Link from "next/link";
+import { StatusBadgeComponent } from "../shared/StatusBadgeComponent";
 
 interface SKUTableProps {
   skus: SKUEntity[];
@@ -8,7 +9,7 @@ interface SKUTableProps {
 export function SKUTableComponent({ skus }: SKUTableProps) {
   return (
     <div className="overflow-x-auto bg-white shadow-sm rounded-lg border">
-      <table className="min-w-full divide-y divide-gray-200">
+      <table className="min-w-full table-auto divide-y divide-gray-200">
         <thead className="bg-gray-50">
           <tr>
             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
@@ -21,7 +22,7 @@ export function SKUTableComponent({ skus }: SKUTableProps) {
               Descrição
             </th>
             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Descrição Comercial
+              Status
             </th>
             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
               Dimensões
@@ -40,45 +41,50 @@ export function SKUTableComponent({ skus }: SKUTableProps) {
         <tbody className="bg-white divide-y divide-gray-200">
           {skus.map((sku) => (
             <tr key={sku.id} className="hover:bg-gray-50">
-              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+              <td className="px-2 py-4 whitespace-nowrap max-w-[50px] gap-2 text-sm font-medium text-gray-900">
                 {sku.skuCode}
               </td>
-              <td className="px-6 py-4 text-sm text-gray-900">
+              <td className="px-4 py-4 text-sm text-gray-900 gap-1">
                 <div>
                   <div className="font-medium">{sku.product.name}</div>
-                  <div className="text-gray-500 text-xs">
-                    {sku.product.description} - {sku.product.brand}
-                  </div>
                 </div>
               </td>
-              <td className="px-6 py-4 text-sm text-gray-900">
-                {sku.description}
+              <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-900">
+                <div className="font-medium">{sku.description}</div>
+                <div className="text-gray-500 text-xs whitespace-normal">
+                  {sku.commercialDescription}
+                </div>
               </td>
-              <td className="px-6 py-4 text-sm text-gray-900">
-                {sku.commercialDescription}
+              <td className="px-4 py-4 whitespace-nowrap">
+                <StatusBadgeComponent status={sku.status as SKUStatusEnum} />
               </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+              <td className="px-4 py-4 whitespace-nowrap max-w-[50px] text-sm text-gray-900">
                 {sku.volumetry.value} {sku.volumetry.unit}
               </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+              <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-900">
                 <div className="font-medium">{sku.packaging.type}</div>
                 <div className="text-gray-500 text-xs">
                   {sku.packaging.material}
                 </div>
               </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+              <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-900">
                 <div className="font-medium">{sku.composition.formula}</div>
                 <div className="text-gray-500 text-xs whitespace-normal">
                   {sku.composition.keyIngredients}
                 </div>
               </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium ">
+              <td className="px-4 py-4 whitespace-nowrap text-sm font-medium flex gap-2">
                 <Link
-                  title="Editar SKU"
-                  href={`/skus/${sku.id}/edit`}
-                  className="text-gray-600 hover:text-gray-900 justify-center"
+                  href={`/skus/${sku.id}/clone`}
+                  className="px-2 py-1 bg-indigo-100 text-indigo-700 rounded hover:bg-indigo-200 transition"
                 >
-                  📝 Editar
+                  Clonar
+                </Link>
+                <Link
+                  href={`/skus/${sku.id}/edit`}
+                  className="px-2 py-1 bg-gray-100 text-gray-700 rounded hover:bg-gray-200 transition"
+                >
+                  Editar
                 </Link>
               </td>
             </tr>
