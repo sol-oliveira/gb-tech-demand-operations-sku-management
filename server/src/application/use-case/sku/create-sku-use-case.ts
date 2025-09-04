7bb5490266e6df594938558521a2ee4dd50e33c6
Sole Oliveira <sole-oliveira@hotmail.com>
2025-09-03 22:32:49 -0300

    feat: adiciona gerador de codigo

--- server/src/application/use-case/sku/create-sku-use-case.ts
+++ server/src/application/use-case/sku/create-sku-use-case.ts
@@ -1,3 +1,5 @@
+import { SKUEntity } from "../../../domain/entities/sku.entity";
+import { SKUStatusEnum } from "../../../domain/enums/sku.enum";
 import { ISKURepository } from "../../../domain/repositories/sku.repository.interface";
 import { SKUCreateInput } from "../../../domain/validation/sku.schema";
 
@@ -12,7 +14,12 @@ export class SKUCreateUseCase {
     skuInput: SKUCreateInput
   ): Promise<{ success: boolean; message: string }> {
     try {
-      const result = await this.skuRepository.create(skuInput);
+      const dataToCreate = {
+        ...skuInput,
+        skuCode: skuInput.skuCode ?? SKUEntity.generateSkuCode(),
+        status: SKUStatusEnum.PRE_CADASTRO,
+      };
+      const result = await this.skuRepository.create(dataToCreate);
       return {
         success: result,
         message: result ? "SKU criado com sucesso" : "Falha ao criar o SKU",
--- server/src/domain/entities/sku.entity.ts
+++ server/src/domain/entities/sku.entity.ts
@@ -1,52 +1,58 @@
-import { ProductEntity } from './product.entity'
-import { CompositionValueObject } from '../value_objects/composition.vo'
-import { VolumetryValueObject } from '../value_objects/volumetry.vo'
-import { PackagingValueObject } from '../value_objects/packaging.vo'
-import { SKUStatusEnum } from '../enums/sku.enum'
+import { ProductEntity } from "./product.entity";
+import { CompositionValueObject } from "../value_objects/composition.vo";
+import { VolumetryValueObject } from "../value_objects/volumetry.vo";
+import { PackagingValueObject } from "../value_objects/packaging.vo";
+import { SKUStatusEnum } from "../enums/sku.enum";
 
+export type SKUEntityType = {
+  id: string;
+  skuCode: string;
+  description: string;
+  commercialDescription: string;
+  status: SKUStatusEnum;
+  product: ProductEntity;
+  composition: CompositionValueObject;
+  volumetry: VolumetryValueObject;
+  packaging: PackagingValueObject;
+  createdAt?: Date;
+  updatedAt?: Date | null;
+  userCreate: string;
+  userUpdate?: string | null;
+};
 
 export class SKUEntity {
-    id: string;
-    skuCode: string;
-    description: string;
-    commercialDescription: string;
-    status: SKUStatusEnum;
-    product: ProductEntity; 
-    composition: CompositionValueObject 
-    volumetry: VolumetryValueObject 
-    packaging: PackagingValueObject 
-    createdAt: Date;
-    updatedAt: Date | null;
-    userCreate: string ; 
-    userUpdate: string | null;
-    
-    constructor(data: {
-        id: string; 
-        skuCode: string; 
-        description: string; 
-        commercialDescription: string;
-        status: SKUStatusEnum;
-        product: ProductEntity; 
-        composition: CompositionValueObject; 
-        volumetry: VolumetryValueObject; 
-        packaging: PackagingValueObject;
-        createdAt: Date; 
-        updatedAt: Date | null; 
-        userCreate: string; 
-        userUpdate: string | null;
-    }) {
-        this.id = data.id;
-        this.skuCode = data.skuCode;
-        this.description = data.description;
-        this.commercialDescription = data.commercialDescription;
-        this.status = data.status;
-        this.product = data.product;
-        this.composition = data.composition;
-        this.volumetry = data.volumetry;
-        this.packaging = data.packaging;
-        this.createdAt = data.createdAt || new Date();
-        this.updatedAt = data.updatedAt || null;
-        this.userCreate = data.userCreate;
-        this.userUpdate = data.userUpdate || null;
-    }    
+  id: string;
+  skuCode: string;
+  description: string;
+  commercialDescription: string;
+  status: SKUStatusEnum;
+  product: ProductEntity;
+  composition: CompositionValueObject;
+  volumetry: VolumetryValueObject;
+  packaging: PackagingValueObject;
+  createdAt: Date;
+  updatedAt: Date | null;
+  userCreate: string;
+  userUpdate: string | null;
+
+  constructor(props: SKUEntityType) {
+    this.id = props.id;
+    this.skuCode = props.skuCode;
+    this.description = props.description;
+    this.commercialDescription = props.commercialDescription;
+    this.status = props.status;
+    this.product = props.product;
+    this.composition = props.composition;
+    this.volumetry = props.volumetry;
+    this.packaging = props.packaging;
+    this.createdAt = props.createdAt ?? new Date();
+    this.updatedAt = props.updatedAt ?? null;
+    this.userCreate = props.userCreate;
+    this.userUpdate = props.userUpdate ?? null;
+  }
+
+  static generateSkuCode(): string {
+    const random = Math.floor(10000 + Math.random() * 90000);
+    return `SKU-${random}`;
+  }
 }
